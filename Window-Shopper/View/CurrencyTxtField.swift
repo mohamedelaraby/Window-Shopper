29b323d1d535dc9f48f8f7dc1545d144ca78b4f0
Muhammad Alaraby <mohamedelaraby1995@gmail.com>
2019-08-27 10:35:44 +0200

    Custom style for textField

--- Window-Shopper/View/CurrencyTxtField.swift
+++ Window-Shopper/View/CurrencyTxtField.swift
@@ -10,12 +10,24 @@ import UIKit
 
 class CurrencyTxtField: UITextField{
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        styleTextField()
     }
-    */
-
+    
+    private func styleTextField(){
+        backgroundColor = #colorLiteral(red: 0.9922674298, green: 0.9842233062, blue: 0.9753331542, alpha: 0.25)
+        layer.cornerRadius = 5.0
+        textAlignment = .center
+        
+        //Place holder
+        if placeholder == nil {
+            placeholder = " "
+        }
+        
+        let placeHolder = NSAttributedString(string: placeholder!, attributes: [.foregroundColor: #colorLiteral(red: 0.9922674298, green: 0.9842233062, blue: 0.9753331542, alpha: 1)])
+        attributedPlaceholder = placeHolder
+    
+        textColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+    }
 }
